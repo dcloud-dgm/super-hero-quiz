5667766cee1bd14b98092090779f3b57e5f73edd
Alek <aleksimpson@icloud.com>
2017-05-11 16:07:49 +0000

    comment

---
+++ README.md
@@ -0,0 +1,7 @@
+1. super-heroe-quiz
+2. dennis,alek,sammy
+3. reflection
+4. i think my team was pretty focused when we needed tobe during the project
+5. i personaly think my team learned alot this year in coding, because only one of our team-mates had ever done coding before this year
+6. we think we deserve a 6 or a seven for our grade 
+7. 
---
+++ app/models/model.rb
@@ -0,0 +1,23 @@
+class SuperHero
+    attr_accessor :name, :fav_color
+    
+    def initialize(name, fav_color)
+        @name = name
+        @fav_color = fav_color
+    end
+    
+    
+    def game
+       $dcmen = ["Superman", "Batman"]
+       $dcwomen = []
+       $dcrobot = []
+       $marvelmen = []
+       $marvelwomen = []
+       $marvelrobot = []
+       $othermen = []
+       $otherwomen  = []
+       $otherrobot = []
+       
+       if 
+    end
+end
